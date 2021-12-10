6b828feea56095636cd5cc3b4f448f392d905707
43-50ph14 <so_s@gmx.de>
2021-12-10 21:56:55 +0100

    simplified settings.cmake

--- dobbyOS/settings.cmake
+++ dobbyOS/settings.cmake
@@ -17,28 +17,28 @@ set(KernelPlatform "spike")
 set(KernelSel4Arch "riscv")
 
 include(/host/kernel/configs/seL4Config.cmake)
-set(CapDLLoaderMaxObjects 20000 CACHE STRING "" FORCE)
-set(KernelRootCNodeSizeBits 16 CACHE STRING "")
+#set(CapDLLoaderMaxObjects 20000 CACHE STRING "" FORCE)
+#set(KernelRootCNodeSizeBits 16 CACHE STRING "")
 
 # For the tutorials that do initialize the plat support serial printing they still
 # just want to use the kernel debug putchar if it exists
-set(LibSel4PlatSupportUseDebugPutChar true CACHE BOOL "" FORCE)
+#set(LibSel4PlatSupportUseDebugPutChar true CACHE BOOL "" FORCE)
 
 # Just let the regular abort spin without calling DebugHalt to prevent needless
 # confusing output from the kernel for a tutorial
-set(LibSel4MuslcSysDebugHalt FALSE CACHE BOOL "" FORCE)
+#set(LibSel4MuslcSysDebugHalt FALSE CACHE BOOL "" FORCE)
 
 # Only configure a single domain for the domain scheduler
-set(KernelNumDomains 1 CACHE STRING "" FORCE)
+#set(KernelNumDomains 1 CACHE STRING "" FORCE)
 
 # We must build the debug kernel because the tutorials rely on seL4_DebugPutChar
 # and they don't initialize a platsupport driver.
-ApplyCommonReleaseVerificationSettings(FALSE FALSE)
+#ApplyCommonReleaseVerificationSettings(FALSE FALSE)
 
 # We will attempt to generate a simulation script, so try and generate a simulation
 # compatible configuration
-ApplyCommonSimulationSettings(${KernelSel4Arch})
-if(FORCE_IOMMU)
-    set(KernelIOMMU ON CACHE BOOL "" FORCE)
-endif()
+#ApplyCommonSimulationSettings(${KernelSel4Arch})
+#if(FORCE_IOMMU)
+#    set(KernelIOMMU ON CACHE BOOL "" FORCE)
+#endif()
 
