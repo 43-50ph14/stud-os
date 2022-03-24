ee888a6588ce40874a0204021f9ed88ae9eb6bb2
43_50ph14 <so_s@gmx.de>
2022-03-24 12:17:24 +0100

    multiple c files to execute setup

---
+++ dobbyOS/projects/Hello/src/io.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int Out(void){
+  printf("Hello from io.c\n");
+  return 0;
+}
--- dobbyOS/projects/Hello/src/main.c
+++ dobbyOS/projects/Hello/src/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include "io.h"
+
 
 int main(void)
 {
     printf("The suffering ended\n");
+    printf("hallo world 2.0\n");
+    Out();
     return 0;
 }
