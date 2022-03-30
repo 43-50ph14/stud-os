0b9300657233c866bcf34fabc2f9b624d5571055
43_50ph14 <so_s@gmx.de>
2022-03-30 15:29:49 +0200

    just because i really need to leave now

---
+++ dobbyOS/projects/Hello/src/thread.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <sel4/sel4.h>
+#include <utils/util.h>
+#include <sel4utils/util.h>
+#include <sel4utils/herpers.h>
+
+int new_thread(void *arg1, void *arg2, void *arg3){
+  
+}
