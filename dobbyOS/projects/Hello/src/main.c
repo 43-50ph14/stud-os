58baf7675e818feef715f11ee857d106a199ce20
43_50ph14 <so_s@gmx.de>
2022-05-06 17:58:48 +0200

    playing around with different parameters. ended up in illegal iperation

--- dobbyOS/projects/Hello/src/main.c
+++ dobbyOS/projects/Hello/src/main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <sel4/sel4.h>
 #include <sel4platsupport/bootinfo.h>
@@ -22,66 +21,71 @@ int main(int argc, char *argv[]) {
     // list of general seL4 object size_bits
     seL4_Word sizes[] = {seL4_TCBBits, seL4_EndpointBits, seL4_NotificationBits};
 
-    // seL4_EndpointBits and seL4_NotificationBits are both less than seL4_TCBBits, which
-    // means that all objects together fit into the size of two TCBs, or 2^(seL4_TCBBits + 1):
-    seL4_Word untyped_size_bits = seL4_TCBBits*3+1;
+    printf("tcb: %d\n", seL4_TCBObject);
+
+    // TODO work out what size object we need to create to be able to create all of the objects
+    // listed above. Remember that sizes are in bits, that is, the exponents of powers of two.
+    seL4_Word untyped_size_bits = seL4_TCBBits+1;
     seL4_CPtr parent_untyped = 0;
     seL4_CPtr child_untyped = info->empty.start;
 
+    printf("cond: %d\n", (info->untyped.end - info->untyped.start));
+
     // First, find an untyped big enough to fit all of our objects
     for (int i = 0; i < (info->untyped.end - info->untyped.start); i++) {
-        printf("i: %d\n", &i);
         if (info->untypedList[i].sizeBits >= untyped_size_bits && !info->untypedList[i].isDevice) {
             parent_untyped = info->untyped.start + i;
-            printf("parent: %d\n", &parent_untyped);
             break;
         }
     }
+    printf("parend: %d\n", parent_untyped);
+    parent_untyped = 56;
+    child_untyped = 0;
     // create an untyped big enough to retype all of the above objects from
+
     error = seL4_Untyped_Retype(parent_untyped, // the untyped capability to retype
                                 seL4_UntypedObject, // type
                                 untyped_size_bits,  //size
-                                seL4_CapInitThreadTCB, // root
+                                seL4_CapInitThreadCNode, // root
                                 0, // node_index
-                                0, // node_depth
+                                32, // node_depth
                                 child_untyped, // node_offset
                                 1 // num_caps
                                 );
-    printf("%d\n", error);
-//    ZF_LOGF_IF(error != seL4_NoError, "Failed to retype");
+   printf("error: %d\n", error); 
+   ZF_LOGF_IF(error != seL4_NoError, "Failed to retype");
 
     // use the slot after child_untyped for the new TCB cap:
     seL4_CPtr child_tcb = child_untyped + 1;
-    seL4_Untyped_Retype(child_untyped, seL4_TCBObject, 0, seL4_CapInitThreadCNode, 0, 0, child_tcb, 1);
+    /* TODO create a TCB in CSlot child_tcb */
 
     // try to set the TCB priority
     error = seL4_TCB_SetPriority(child_tcb, seL4_CapInitThreadTCB, 10);
- //   ZF_LOGF_IF(error != seL4_NoError, "Failed to set priority");
+    ZF_LOGF_IF(error != seL4_NoError, "Failed to set priority");
 
     // use the slot after child_tcb for the new endpoint cap:
     seL4_CPtr child_ep = child_tcb + 1;
-    seL4_Untyped_Retype(child_untyped, seL4_EndpointObject, 0, seL4_CapInitThreadCNode, 0, 0, child_ep, 1);
+    /* TODO create an endpoint in CSlot child_ep */
 
     // identify the type of child_ep
     uint32_t cap_id = seL4_DebugCapIdentify(child_ep);
-   // ZF_LOGF_IF(cap_id == 0, "Endpoint cap is null cap");
+    ZF_LOGF_IF(cap_id == 0, "Endpoint cap is null cap");
 
     // use the slot after child_ep for the new notification cap:
     seL4_CPtr child_ntfn = child_ep + 1;
-    seL4_Untyped_Retype(child_untyped, seL4_NotificationObject, 0, seL4_CapInitThreadCNode, 0, 0, child_ntfn, 1);
+    // TODO create a notification object in CSlot child_ntfn
 
     // try to use child_ntfn
     error = seL4_TCB_BindNotification(child_tcb, child_ntfn);
-    //ZF_LOGF_IF(error != seL4_NoError, "Failed to bind notification.");
+    ZF_LOGF_IF(error != seL4_NoError, "Failed to bind notification.");
 
-    error = seL4_CNode_Revoke(seL4_CapInitThreadCNode, child_untyped, seL4_WordBits);
-    assert(error == seL4_NoError);
+    // TODO revoke the child untyped
 
     // allocate the whole child_untyped as endpoints
     // Remember the sizes are exponents, so this computes 2^untyped_size_bits / 2^seL4_EndpointBits:
     seL4_Word num_eps = BIT(untyped_size_bits - seL4_EndpointBits);
     error = seL4_Untyped_Retype(child_untyped, seL4_EndpointObject, 0, seL4_CapInitThreadCNode, 0, 0, child_tcb, num_eps);
-   // ZF_LOGF_IF(error != seL4_NoError, "Failed to create endpoints.");
+    ZF_LOGF_IF(error != seL4_NoError, "Failed to create endpoints.");
 
     printf("Success\n");
 
